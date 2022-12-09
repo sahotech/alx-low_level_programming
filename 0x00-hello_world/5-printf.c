c4b8141b2aa6a847f1eec13284764d82de946685
sahotech <sahotech06gmail.com>
2022-12-09 00:12:39 -0800

    low level

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - Entry                                                                                                                         
- * Return: Always 0                                                                                                                      
- */ 
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * return - Always return 0.
+ */
 int main(void)
- 
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
