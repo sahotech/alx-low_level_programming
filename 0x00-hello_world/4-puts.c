ff44cb2abb43f670b92f4935cd83a8c4f9878adc
sahotech <sahotech06gmail.com>
2022-12-08 16:23:03 -0800

    Low Level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */                                                                                                                                     
 int main(void)                                                                                                                          
 {                                                                                                                                       
-        puts("\"Programming is like building a multi lingual puzzle");                                                            
+        puts("\"Programming is like building a multi lingual puzzle);                                                            
         return (0);
 }
 
