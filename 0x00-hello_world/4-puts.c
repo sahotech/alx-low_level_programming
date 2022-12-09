a6db2a4ba8cb43ad003f276aef48dc6558e07b48
sahotech <sahotech06gmail.com>
2022-12-09 01:06:08 -0800

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Entry point
  * return - Always return 0.
  */
-int main(void)
+int main(void) /* Function */
 {
 	puts(”\"Programming is like building a multilingual puzzle");
 	return (0);
