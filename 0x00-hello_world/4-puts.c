405b4256c76d475ea571e4858f42515567111155
sahotech <sahotech06gmail.com>
2022-12-08 16:35:04 -0800

    Low Level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>                                                                     /**                                                                                                                                     
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
  * main - Entry                                                                                                                         
  * Return: Always 0                                                                                                                     
- */                                                                                                                                   
-int main(void)                                                    
-{                                                                                                                                       
-	puts("\"Programming is like building a multilingual puzzle");                                                            
+ */                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                 
+        puts("\"Programming is like building a multilingual puzzle");                                                            
         return (0);
 }
+
 
