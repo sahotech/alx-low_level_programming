8cef26e5bc02aef9d91cba666a69fe4a1f76a4f0
sahotech <sahotech06gmail.com>
2022-12-08 16:14:20 -0800

    Low Level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #iclude <stdio.h>
 int main (void)
+	/*
+	 * Main, is the function
+	 *  
+	 */ 
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
