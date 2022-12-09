0fe49a3e50223f09dc4249daf6b00191a3f64b06
sahotech <sahotech06gmail.com>
2022-12-08 16:47:17 -0800

    Low Level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
  * main - Entry of the function
  * return 0 if the code is correct
  */
+
  int main(void) 
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+	}	
 
 
