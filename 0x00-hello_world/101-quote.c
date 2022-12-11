c566aca5ba2c584a39566988cbacb2157d7b6bed
sahotech <sahotech06gmail.com>
2022-12-11 06:31:29 -0800

    Advance Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
- * main-Entry point
- * Return: Always 1 (success)
- */
+* main-Entry point
+* Return: Always 1 (success)
+*/
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-
