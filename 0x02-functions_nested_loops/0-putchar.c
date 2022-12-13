d3eaf9de1200e7ef553a025fe1f6ad5c30f2945c
sahotech <sahotech06gmail.com>
2022-12-13 14:39:11 -0800

    notthing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/** main - Entry point
+ * Description - Print outthe _putchar
+ * return - it return 0 always
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
