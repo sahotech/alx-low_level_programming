48b19250b725be58f1a593e2acf4cf3b3e7c84c0
sahotech <sahotech06gmail.com>
2022-12-13 14:47:15 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description - print out alphabet in lowercase
+ * return - 0 always
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 0; c <= z; c++);
+	putchar(c);
+	putchar(\n);
+	return (0);
+}
