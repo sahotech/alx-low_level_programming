7ad62bfbba5aa0b85bcdd2975c861f602e811bd3
sahotech <sahotech06gmail.com>
2022-12-15 13:21:06 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upperrcase character
+ * *@c: is the int that will use for the argument of the function
+ * Return:0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
