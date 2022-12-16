9e00f6ea1ea049114e5a2529f197e68c561f5fd9
sahotech <sahotech06gmail.com>
2022-12-15 16:48:16 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ */
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 28; i < 38; i++)
+{
+if (i != 30 && i != 35)
+}
+_putchar(i);
+}
+}
+_putchar('\n');
+}
