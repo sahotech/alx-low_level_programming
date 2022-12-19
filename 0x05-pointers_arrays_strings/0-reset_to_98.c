1ff60ec76d3540e75ff6779745f9d1d87d41561e
sahotech <sahotech06gmail.com>
2022-12-19 12:54:04 -0800

    Pointers. Arrays and String in C

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.hh"
+
+/**
+ * reset_to_98 = update the value to the Variable
+ * the pointer points to 98
+ * @n: pointer to the variable
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
