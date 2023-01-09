c24c7e55d91384ae4fde91c701f952b1fb618aec
sahotech <sahotech06gmail.com>
2023-01-09 11:20:18 -0800

    tasks 1 to advances

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints all arguments it receives.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
-  * Return: 0 - success.
-  */
+ * Return: 0 - success.
+ */
 
 int main(int argc, char *argv[])
 {
